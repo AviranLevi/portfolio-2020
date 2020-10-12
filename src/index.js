461125835d6fd7ebf0c54335502fdb44b0074242
Aviran Levi <aveloofficial@gmail.com>
2020-10-12 14:49:36 +0300

    prepare to deploy

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import router from './router';
+import path from 'path';
 
 const app = express();
 const port = process.env.SERVER_PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.use('/api', router);
 
