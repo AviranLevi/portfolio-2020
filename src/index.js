fdb1548aa8dbc023b1a1c526b08526dad256d746
Aviran Levi <aveloofficial@gmail.com>
2020-10-12 15:29:51 +0300

    proxy + server minor update

--- src/index.js
+++ src/index.js
@@ -9,6 +9,9 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, '../client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '../client/public/index.html'));
+});
 
 app.use('/api', router);
 
