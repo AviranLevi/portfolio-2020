c5fef51215533c6338819495572136bb4d827a76
Aviran Levi <aveloofficial@gmail.com>
2020-10-12 14:21:55 +0300

    config - server url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import { serverURL } from '../config';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api/',
+  baseURL: serverURL,
 });
 
 export const sendEmail = async (userInfo = {}) => {
