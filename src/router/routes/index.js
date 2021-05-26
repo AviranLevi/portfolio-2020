6a83e90310a1daa3aec178e2d608d0ce3b481572
AviranLevi <aveloofficial@gmail.com>
2021-05-26 14:41:29 +0300

    minor server update

--- src/router/routes/index.js
+++ src/router/routes/index.js
@@ -1,28 +1,28 @@
-import nodemailer from 'nodemailer';
-import { mailerINFO } from '../../config';
+import nodemailer from 'nodemailer'
+import { mailerINFO } from '../../config'
 
-const { service, user, pass, from, to } = mailerINFO;
+const { service, user, pass, from, to } = mailerINFO
 
-const transporter = nodemailer.createTransport({
+const transporter = nodemailer.createTransport('SMTP', {
   service,
-  host: 'smtp.gmail.com',
   auth: { user, pass },
   tls: { rejectUnauthorized: false },
-});
+})
 
 export const sendEmail = async (req, res, next) => {
   try {
-    const { name, email, text } = req.body;
-    const subject = `${name} - ${email}`;
-    await transporter.sendMail({ from, to, subject, text }, (error, info) => {
+    const { name, email, text } = req.body
+    const subject = `${name} - ${email}`
+    const mailOptions = { from, to, subject, text }
+    await transporter.sendMail(mailOptions, (error, info) => {
       if (error) {
-        console.log('****error****', error);
-        res.json({ success: false, error });
+        console.log('****error****', error)
+        res.json({ success: false, error })
       } else {
-        res.json({ success: true, message: 'Email sent!' });
+        res.json({ success: true, message: 'Email sent!' })
       }
-    });
+    })
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
-};
+}
